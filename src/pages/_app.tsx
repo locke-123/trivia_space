0fdba6356a3d9b34e11294df2c9d20970e993e70
locke-123 <113772136+locke-123@users.noreply.github.com>
2023-09-25 04:17:09 +0900

    initial setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
+import { ConfigProvider } from 'antd';
 import type { AppProps } from 'next/app'
+import theme_basic from '@/styles/theme/themeConfig';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ConfigProvider theme={theme_basic}>
+      <Component {...pageProps} />
+    </ConfigProvider>
+  )
 }
---
+++ src/styles/theme/themeConfig.ts
@@ -0,0 +1,10 @@
+import type { ThemeConfig } from 'antd';
+
+const theme_basic: ThemeConfig = {
+  token: {
+    fontSize: 16,
+    colorPrimary: '#895fad',
+  },
+};
+
+export default theme_basic;
